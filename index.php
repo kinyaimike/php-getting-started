756186758932a9b7d3b25de0c4c771e868fb1143
kinyaimike <mikemicsprosounders@gmail.com>
2022-11-10 13:17:32 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo"hello world program ";
+
+$myName ="michaelkinyai";
+echo $myName;
+var_dump($myName);
+
+// integers
+$a=30;
+$b="30";
+$c=true;
+$d=200.34;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+
+echo "my Name is ".$myName. " My M-pesa balance is ksh ".$c;
+
+?>
